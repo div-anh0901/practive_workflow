84e73ba121e89fdbb43d276b94f8da8829db048c
mac <mac@macs-MBP.lan>
2025-03-18 10:22:36 +0700

    fix package not foundl

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const request = require('supertest');
-const app = require('./index');
+const request = require('supertest')
+const app = require('./index')
 
 describe('GET /', () => {
   it('responds with Hello, World!', async () => {
